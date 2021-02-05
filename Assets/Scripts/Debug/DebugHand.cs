e6e28accae3f8c69d5c49b85bb9850867d988c63
Will Lacey <WillLacey@Will-Laceys-MacBook-Pro-4.local>
2021-02-05 16:03:37 -0600

    Unit Path Updates

---
+++ Assets/Scripts/Debug/DebugHand.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugHand : MonoBehaviour
+{
+    /************************************************************/
+    #region Variables
+
+    [Tooltip("layers to ignore when raycasting")]
+    [SerializeField] LayerMask layersToIgnore; 
+
+    #endregion
+    /************************************************************/
+    #region Unity Functions
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) TryToPickupPiece();
+    }
+
+    #endregion
+
+    /************************************************************/
+    #region Class Functions
+
+    private void TryToPickupPiece()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (!Physics.Raycast(ray, out hit, 1000, ~layersToIgnore)) return;
+
+        Debug.DrawLine(ray.origin, hit.point, Color.red, 3f);
+
+        Debug.Log(hit.rigidbody.transform.parent.name);
+    }
+
+    #endregion
+}
